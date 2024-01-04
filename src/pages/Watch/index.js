d8dd21426c23254917b029182e26a0b6f5bf7842
danieldelimajj <111375723+danieldelimajj@users.noreply.github.com>
2024-01-04 20:58:58 -0300

    adicionei a lib React Router DOM

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,10 +1,19 @@
-import styles from "./Watch.module.css";
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import styles from "./Watch.module.css" 
 
 function Watch() {
     return(
-        <div>
-            <h1>Olá</h1>
-        </div>
+        <>
+            <Header />
+            <Container>
+                <section className={styles.watch}>
+                <iframe width="854" height="480" src="https://www.youtube.com/embed/KsFLXOTflsM?si=kPmYh87GgMwAdVpf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </section>
+            </Container>
+            <Footer />
+        </>
 
     );
 }

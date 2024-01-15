a070d6f838ade0be699cc2690d7fabb1fa5a388c
danieldelimajj <111375723+danieldelimajj@users.noreply.github.com>
2024-01-15 12:48:55 -0300

    finalizei a parte de adicionar vídeos favoritos

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -2,16 +2,20 @@ import styles from './Favorites.module.css';
 import Header from '../../components/Header';
 import Container from '../../components/Container';
 import Footer from '../../components/Footer';
+import VideoList from '../../components/VideoList'
+import { useFavoriteContext } from '../../contexts/Favorites';
 
 
 function Favorites() {
+
+    const { favorite } = useFavoriteContext() 
     return(
         <>
             <Header />
             <Container>
                 <section className={styles.favorites}>
                  <h2>Meus Favoritos</h2>
-                    Lista de favoritos
+                   { <VideoList videos={favorite} /> }
                 </section>
             </Container>
             <Footer />

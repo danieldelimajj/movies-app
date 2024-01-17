65ff7433e59703dd00bd983dd9bcb61af9b5e1f0
danieldelimajj <111375723+danieldelimajj@users.noreply.github.com>
2024-01-17 19:05:27 -0300

    conclui o armazenamento dos dados do formulário

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -8,17 +8,45 @@ function Form() {
     const [category, setCategory] = useState('')
     const [videos, setVideos] = useState([])
     const [errors, setErrors] = useState('')
+
+    function valideUrl(url) {
+        const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/
+
+        if(!regex.test(url) || url.length < 43) {
+            setErrors('ERRO: URL inválida"')
+            return false
+        } else {
+            return url.substring(32, 43) //id do video
+        }
+    }
     
     function onSave(e) {
         e.preventDefault() 
         console.log(url, category)
-        //guardar a URL e a categoria
+
+           //validar category
+           if(!category || category === '-') {
+            setErrors('ERRO: Escolha uma categoria!')
+            return
+        } else {
+            setErrors('')
+        }
+
+        //validar url
+        const urlVideo = valideUrl(url)
+        if(urlVideo && category) {
+             //guardar a URL e a category
         const newVideo = {url, category}
         setVideos([...videos, newVideo])
         localStorage.setItem('videos', JSON.stringify([...videos, newVideo]))
         //limpar o form
         setUrl('')
-        setCategory('')
+        setCategory('ERRO: URL inválida!')
+        } else {
+            setErrors('')
+        }
+
+       
     }
 
     return(
@@ -32,6 +60,8 @@ function Form() {
                         placeholder="Digite a URL do vídeo"
                         required="required"
                         value={url}
+                        minLength='43'
+                        maxLength='43'
                         onChange={e => setUrl(e.target.value)}
                     />
                 </div>
@@ -50,6 +80,9 @@ function Form() {
                 </div>
                 <div>
                     <button>Cadastrar</button>
+                </div>
+                <div>
+                    {errors}
                 </div>
             </form>
         </section>
